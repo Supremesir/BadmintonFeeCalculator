19d4645fc52c920a08db6add8a33a129ed614080
fanghaofan <fanghaofan@wps.cn>
2024-03-08 17:45:52 +0800

    feat: use edgeToEdge api(didn't accomplish the goal though)

--- app/src/main/java/com/supremesir/badmintonfeecalculator/MainActivity.kt
+++ app/src/main/java/com/supremesir/badmintonfeecalculator/MainActivity.kt
@@ -3,14 +3,17 @@ package com.supremesir.badmintonfeecalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -38,11 +41,12 @@ import com.supremesir.badmintonfeecalculator.ui.theme.BadmintonFeeCalculatorThem
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             BadmintonFeeCalculatorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.safeDrawingPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     CalculatorScreen()
