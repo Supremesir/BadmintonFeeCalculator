b5bfe72cf550ac48ba6d8851ce672b8737e669f1
fanghaofan <fanghaofan@wps.cn>
2024-04-15 22:03:49 +0800

    feat: add new picker https://github.com/suzdaleva/ComposeNumberPicker

--- app/src/main/java/com/supremesir/badmintonfeecalculator/ui/theme/Color.kt
+++ app/src/main/java/com/supremesir/badmintonfeecalculator/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.supremesir.badmintonfeecalculator.ui.theme
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +11,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val spinnerGradient = Brush.linearGradient(
+    0.0f to Color(0xFFFFFFFF),
+    0.3f to Color(0x00000000),
+    0.5f to Color(0x00000000),
+    0.7f to Color(0x00000000),
+    1.0f to Color(0xFFFFFFFF),
+    start = Offset(0.0f, -13.0f),
+    end = Offset(0.0f, 320.0f)
+)
